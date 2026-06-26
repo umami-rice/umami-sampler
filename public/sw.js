d976d7858cb4f0bc734505650a3d18712b4ab689
umami-rice <arnabroy1996@gmail.com>
2026-06-26 11:35:35 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = "umami-v1";
+const ASSETS = ["/", "/index.html", "/static/js/main.chunk.js"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
